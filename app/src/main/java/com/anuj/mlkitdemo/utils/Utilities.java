2847810433d14f3ce62e8eef077860be798c10f5
Anuj Gupta <anujgpt32@gmail.com>
2018-08-05 17:12:59 +0530

    Bug Fixes

--- app/src/main/java/com/anuj/mlkitdemo/utils/Utilities.java
+++ app/src/main/java/com/anuj/mlkitdemo/utils/Utilities.java
@@ -1,35 +1,37 @@
 package com.anuj.mlkitdemo.utils;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 
-import java.io.ByteArrayOutputStream;
+import com.anuj.mlkitdemo.R;
 
 /**
  * Created by Anuj on 04-Aug-18.
  */
 public class Utilities {
 
-    /*
-     *   converts bytes to Bitmap
+    /**
+     * Converts byte[] to Bitmap.
+     *
+     * @param image: byte[]
+     * @return Bitmap
      */
     public static Bitmap convertBytesToBitmap(byte[] image) {
         return BitmapFactory.decodeByteArray(image, 0, image.length);
     }
 
-    public static byte[] compressImage(byte[] input, int quality) {
-        Bitmap uncompressedImg = convertBytesToBitmap(input);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        uncompressedImg.compress(Bitmap.CompressFormat.JPEG, quality, out);
-        byte[] result = out.toByteArray();
-        uncompressedImg.recycle();
-        return result;
-    }
 
-    public static String generateImagePath() {
+    /**
+     * Function to generate image name while saving.
+     *
+     * @param activity: for getting the app name.
+     * @return path: path of the image.
+     */
+    public static String generateImagePath(Activity activity) {
         String base = Environment.getExternalStorageDirectory().getPath();
-        String dir = "MlKit";
+        String dir = activity.getString(R.string.app_name);
         return base.concat("/").concat(dir).concat("/").concat(String.valueOf(System.currentTimeMillis())).concat(".jpg");
     }
 
